e9ddb39b4802ae996ff427f2068f33c16a064d26
Art Todorov <art.todorov@me.com>
2020-01-09 14:11:09 -0800

    adding project pages, changing colors, updating some images

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,9 +1,3 @@
-/*
-	Forty by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 (function($) {
 
 	var	$window = $(window),
@@ -374,4 +368,7 @@ $('.read-more').on('click', function() {
 		$(this).text("Read More");
 		$(this).prop('class', 'read-more button fit small icon solid fa-chevron-down');
 	};
-  });
+	});
+	
+// Copyright
+$("#year").text(new Date().getFullYear());
